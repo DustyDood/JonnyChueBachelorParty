995f49b449a418f68e58bcaf5e3b40f36a2ab61d
Dusty Zoland <DikaiosuneZ@gmail.com>
2025-09-09 22:04:22 -0700

    DZ - Initial Push

---
+++ JonnyChueBachelorParty/wwwroot/js/geolocation.js
@@ -0,0 +1,22 @@
+﻿window.geolocationInterop = {
+    getCurrentPosition: function (options, dotNetHelper) {
+        if (!navigator.geolocation) {
+            dotNetHelper.invokeMethodAsync('ReceiveError', 'Geolocation is not supported.');
+            return;
+        }
+
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+                dotNetHelper.invokeMethodAsync('ReceivePosition', {
+                    Latitude: position.coords.latitude,
+                    Longitude: position.coords.longitude,
+                    Accuracy: position.coords.accuracy
+                });
+            },
+            function (error) {
+                dotNetHelper.invokeMethodAsync('ReceiveError', error.message);
+            },
+            options
+        );
+    }
+};
